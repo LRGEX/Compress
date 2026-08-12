b824e7b0c721184c633dc7e78c9719ef01b9438b
lrg4you <lrg.no2@gmail.com>
2026-08-12 20:24:35 +0300

    choco: bump to v1.7.1

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -8,8 +8,8 @@ if (-not [Environment]::Is64BitOperatingSystem) {
 $packageArgs = @{
   packageName    = 'lrgex-compress'
   fileType       = 'exe'
-  url64bit       = 'https://download.lrgex.com/app/rst/lrgex-compress/LRGEX-Compress-v1.7.0-setup.exe'
-  checksum64     = 'f1cb2d698f64661031f6531b045973750727f76f06d5e2597b4712e8885f655a'
+  url64bit       = 'https://download.lrgex.com/app/rst/lrgex-compress/LRGEX-Compress-v1.7.1-setup.exe'
+  checksum64     = '1ecb1ccd55e4085f74667298ba2d1c48f652cbe8b7b7a60d8a53fe854690846e'
   checksumType64 = 'sha256'
   softwareName   = 'LRGEX Compress*'
   silentArgs     = '/VERYSILENT /NORESTART /NOCANCEL /SP- /ALLUSERS'
